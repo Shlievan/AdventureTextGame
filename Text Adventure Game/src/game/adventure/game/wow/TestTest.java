872424168424f7b23ea28ef452534e7fb29ecab6
Jay <Dhruv@10.0.0.15>
2014-08-12 15:38:19 -0700

    Initial "commit" test

---
+++ Text Adventure Game/src/game/adventure/game/wow/TestTest.java
@@ -0,0 +1,13 @@
+package game.adventure.game.wow;
+
+public class TestTest {
+
+	public static void main(String[] args) {
+		displaySomething("Wow though. Like seriously. WOW.");
+
+	}
+	private static void displaySomething(String something){
+		System.out.println(something);
+	}
+
+}
