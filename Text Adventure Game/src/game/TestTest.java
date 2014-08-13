4f62cf9d9a1b33892e264419808a498a9b6eaa35
Jay <Dhruv@10.0.0.15>
2014-08-12 17:49:25 -0700

    wow

--- Text Adventure Game/src/game/TestTest.java
+++ Text Adventure Game/src/game/TestTest.java
@@ -4,8 +4,8 @@ public class TestTest {
 
 	public static void main(String[] args) {
 		displaySomething("Wow though. Like seriously. WOW.");
-		displaySomething("Wait, now I've changed something...");
-		// Seriously though, wtf
+		displaySomething("Wow");
+		
 	}
 	private static void displaySomething(String something){
 		System.out.println(something);
