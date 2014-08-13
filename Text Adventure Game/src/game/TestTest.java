811cd1c64ceb91d380dbd82348c3dac85fef96d7
Jason <Jason@10.0.0.14>
2014-08-12 20:31:59 -0400

    No change

--- Text Adventure Game/src/game/TestTest.java
+++ Text Adventure Game/src/game/TestTest.java
@@ -5,6 +5,7 @@ public class TestTest {
 	public static void main(String[] args) {
 		displaySomething("Wow though. Like seriously. WOW.");
 		displaySomething("Wait, now I've changed something...");
+		// Seriously though, wtf
 	}
 	private static void displaySomething(String something){
 		System.out.println(something);
